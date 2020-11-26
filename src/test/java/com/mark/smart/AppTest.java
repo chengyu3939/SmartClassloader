8cd66999ccee6eedbb1389d22efb77ad896ea470
machaoma <88501614@qq.com>
2020-11-26 19:40:35 +0800

    Initial commit

---
+++ src/test/java/com/mark/smart/AppTest.java
@@ -0,0 +1,20 @@
+package com.mark.smart;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
